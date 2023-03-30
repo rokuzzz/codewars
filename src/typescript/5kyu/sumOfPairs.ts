834b83cef6a9797ddcc9ba5e0d0a507794a514de
Roman Kuzero <rombeldan@gmail.com>
2023-03-30 16:12:54 +0300

    Solve «Sum of Pairs» challenge from codewars.com (optimized solution).‎

--- src/typescript/5kyu/sumOfPairs.ts
+++ src/typescript/5kyu/sumOfPairs.ts
@@ -1,39 +1,51 @@
-export function sumPairs(ints: number[], s: number): [number, number] | void {  
-  // find all pairs that give the given sum by addition, 
-  // store these pairs in the allPairs array, 
-  // and store all indexes of the second elements of
-  // these pairs in the allIndexesOfTheSecondElements array
-  const allPairs: number[][] = [], 
-        allIndexesOfTheSecondElements: number[] = [], 
-        intsLength = ints.length
-  for (let i = 0; i < intsLength; i++) {
-    for (let j = 0; j < intsLength; j++) {
-      if (i !== j && j > i) {
-        if (ints[i] + ints[j] == s) {
-          allPairs.push([ints[i], ints[j]])
-          allIndexesOfTheSecondElements.push(j)
-        }
-      }
+export function sumPairs(ints: number[], targetSum: number): [number, number] | void {  
+  // linear time complexity, O(n)
+  const hashTable = new Map();
+  for (let i in ints) {
+    const firstElement = targetSum - ints[i] 
+    if (hashTable.get(firstElement)) {
+      return [ firstElement, ints[i] ]
+    } else {
+      hashTable.set(ints[i], i)
     }
   }
   
-  // if there are no pairs giving the given sum by addition,
-  // return undefined
-  if (allPairs.length == 0) return undefined
+  // Straight-forward approach, O(n^2)
+  // // find all pairs that give the given sum by addition, 
+  // // store these pairs in the allPairs array, 
+  // // and store all indexes of the second elements of
+  // // these pairs in the allIndexesOfTheSecondElements array
+  // const allPairs: number[][] = [], 
+  //       allIndexesOfTheSecondElements: number[] = [], 
+  //       intsLength = ints.length
+  // for (let i = 0; i < intsLength; i++) {
+  //   for (let j = 0; j < intsLength; j++) {
+  //     if (i !== j && j > i) {
+  //       if (ints[i] + ints[j] == s) {
+  //         allPairs.push([ints[i], ints[j]])
+  //         allIndexesOfTheSecondElements.push(j)
+  //       }
+  //     }
+  //   }
+  // }
   
-  // get second elemen with the lowest index
-  const secondElementWithLowestIndex: number = ints[Math.min(...allIndexesOfTheSecondElements)]
+  // // if there are no pairs giving the given sum by addition,
+  // // return undefined
+  // if (allPairs.length == 0) return undefined
   
-  const allPairsLength = allPairs.length
-  for (let i = 0; i < allPairsLength; i++) {
-    if (allPairs[i][1] == secondElementWithLowestIndex) {
-      const result: [number, number] = [0, 0]
-      for (let j = 0; j < 2; j++) {
-        result.push(allPairs[i][j])
-      }
-      result.splice(0, 2)
+  // // get second element with the lowest index
+  // const secondElementWithLowestIndex: number = ints[Math.min(...allIndexesOfTheSecondElements)]
+  
+  // const allPairsLength = allPairs.length
+  // for (let i = 0; i < allPairsLength; i++) {
+  //   if (allPairs[i][1] == secondElementWithLowestIndex) {
+  //     const result: [number, number] = [0, 0]
+  //     for (let j = 0; j < 2; j++) {
+  //       result.push(allPairs[i][j])
+  //     }
+  //     result.splice(0, 2)
       
-      return result
-    }
-  }
+  //     return result
+  //   }
+  // }
 }
