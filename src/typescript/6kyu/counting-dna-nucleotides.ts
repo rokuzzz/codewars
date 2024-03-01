b1a3014a147ce298cbebdfb05feab6b8298157f7
Roman Kuzero <rombeldan@gmail.com>
2024-03-01 23:46:54 +0200

    Add description to «Counting DNA nucleotides» codewars challenge

--- src/typescript/6kyu/counting-dna-nucleotides.ts
+++ src/typescript/6kyu/counting-dna-nucleotides.ts
@@ -1,3 +1,16 @@
+/**
+ * Name: Counting DNA Nucleotides
+ *
+ * Description:
+ * For a given DNA genetic code represented by a string, count the number of times the
+ * letters A (adenine), C (cytosine), G (guanine) and T (thymine) appears and return and
+ * object.
+ *
+ * The input string may contain both upper and lower case characters.
+ *
+ * Level: 6 kyu
+ */
+
 function getCountedNucleotides(genCode: string) {
   const nucleotidesArray = genCode.toUpperCase().split('');
   const nucleotideFrequencies: { [key: string]: number } = {
