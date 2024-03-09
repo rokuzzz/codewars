7fb19be7ee1dbe419ff5d39edac5790605736e9a
Roman Kuzero <rombeldan@gmail.com>
2024-03-09 18:12:54 +0200

    Solve «Keep Hydrated!» codewars challenge

---
+++ src/typescript/8kyu/keep-hydrated.ts
@@ -0,0 +1,23 @@
+/**
+ * Name: Beginner Series #3 Sum of Numbers
+ *
+ * Description:
+ * Nathan loves cycling.
+ *
+ * Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per
+ * hour of cycling.
+ *
+ * You get given the time in hours and you need to return the number of litres Nathan will
+ * drink, rounded to the smallest value.
+ *
+ * Level: 8 kyu
+ */
+
+export function litres(time: number): number {
+  return Math.floor(time * 0.5);
+}
+
+// Test cases
+console.log(litres(3)); // 1
+console.log(litres(6.7)); // 3
+console.log(litres(11.8)); // 5
